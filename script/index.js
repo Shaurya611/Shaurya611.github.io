6d32ad092e0fda4954942c3d96a5bc1f26202379
Shaurya Kumar <bobbyseth94@gmail.com>
2023-03-18 15:08:02 +0530

    doing responsive work

--- script/index.js
+++ script/index.js
@@ -1,5 +1,16 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector(".Navbar_div");
-    header.classList.toggle("sticky", window.scrollY > 0);
+// window.addEventListener("scroll", function(){
+//     let header = document.querySelector(".Navbar_div");
+//     header.classList.toggle("sticky", window.scrollY > 0);
+// })
+
+let burger_icon = document.querySelector(".ham_icon");
+burger_icon.addEventListener("click", () =>{
+    let navbar =document.querySelector(".burger_icons");
+    navbar.style.display="block";
 })
 
+let cross = document.querySelector(".cross");
+cross.addEventListener("click", ()=>{
+    let navbar =document.querySelector(".burger_icons");
+    navbar.style.display="none";
+})
